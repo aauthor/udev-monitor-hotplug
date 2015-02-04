cfb3094f027849211a9a8ffaf5ab9fdea994c66e
Zhuoyun Wei <wzyboy@wzyboy.org>
2015-02-04 14:36:44 +0800

    Adaptation

--- usr/local/bin/monitor-hotplug.sh
+++ usr/local/bin/monitor-hotplug.sh
@@ -1,69 +1,26 @@
-#!/bin/bash
+#!/bin/bash -
 
-#Adapt this script to your needs.
-
-DEVICES=$(find /sys/class/drm/*/status)
-
-#inspired by /etc/acpd/lid.sh and the function it sources
+INTNAME="eDP1"
+#INTSTATUS="connected"
+EXTNAME="DP1"
+EXTSTATUS=$(cat /sys/class/drm/card0-DP-1/status)
 
 displaynum=`ls /tmp/.X11-unix/* | sed s#/tmp/.X11-unix/X##`
 display=":$displaynum"
 export DISPLAY=":$displaynum"
 
-uid=$(ck-list-sessions | awk 'BEGIN { unix_user = ""; } /^Session/ { unix_user = ""; } /unix-user =/ { gsub(/'\''/,"",$3); unix_user = $3; } /x11-display = '\'$display\''/ { print unix_user; exit (0); }')
+uid=$(loginctl list-users | grep -Po '\d{4}') # Assuming uid >= 1000 #FIXME
 if [ -n "$uid" ]; then
-	# from https://wiki.archlinux.org/index.php/Acpid#Laptop_Monitor_Power_Off
-	export XAUTHORITY=$(ps -C Xorg -f --no-header | sed -n 's/.*-auth //; s/ -[^ ].*//; p')
+  export XAUTHORITY=$(ps -C Xorg.bin -f --no-header | sed -n 's/.*-auth //; s/ -[^ ].*//; p')
 else
   echo "unable to find an X session"
   exit 1
 fi
 
-
-#this while loop declare the $HDMI1 $VGA1 $LVDS1 and others if they are plugged in
-while read l 
-do 
-  dir=$(dirname $l); 
-  status=$(cat $l); 
-  dev=$(echo $dir | cut -d\- -f 2-); 
-  
-  if [ $(expr match  $dev "HDMI") != "0" ]
-  then
-#REMOVE THE -X- part from HDMI-X-n
-    dev=HDMI${dev#HDMI-?-}
-  else 
-    dev=$(echo $dev | tr -d '-')
-  fi
-
-  if [ "connected" == "$status" ]
-  then 
-    echo $dev "connected"
-    declare $dev="yes"; 
-  
-  fi
-done <<< "$DEVICES"
-
-
-if [ ! -z "$HDMI1" -a ! -z "$VGA1" ]
-then
-  echo "HDMI1 and VGA1 are plugged in"
-  xrandr --output LVDS1 --off
-  xrandr --output VGA1 --mode 1920x1080 --noprimary
-  xrandr --output HDMI1 --mode 1920x1080 --right-of VGA1 --primary
-elif [ ! -z "$HDMI1" -a -z "$VGA1" ]; then
-  echo "HDMI1 is plugged in, but not VGA1"
-  xrandr --output LVDS1 --off
-  xrandr --output VGA1 --off
-  xrandr --output HDMI1 --mode 1920x1080 --primary
-elif [ -z "$HDMI1" -a ! -z "$VGA1" ]; then
-  echo "VGA1 is plugged in, but not HDMI1"
-  xrandr --output LVDS1 --off
-  xrandr --output HDMI1 --off
-  xrandr --output VGA1 --mode 1920x1080 --primary
-else
-  echo "No external monitors are plugged in"
-  xrandr --output LVDS1 --off
-  xrandr --output HDMI1 --off
-  xrandr --output LVDS1 --mode 1366x768 --primary
+if [[ $EXTSTATUS = "connected" ]]; then
+  xrandr --output $INTNAME --off --output $EXTNAME --auto
+  nitrogen --restore
+elif [[ $EXTSTATUS = "disconnected" ]]; then
+  xrandr --output $INTNAME --auto --output $EXTNAME --off
+  nitrogen --restore
 fi
-
