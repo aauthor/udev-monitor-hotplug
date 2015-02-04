10420b5323c29cf93d9581644d5ca87a1a634674
Zhuoyun Wei <wzyboy@wzyboy.org>
2015-02-04 15:37:23 +0800

    Code cleaning up

--- usr/local/bin/monitor-hotplug.sh
+++ usr/local/bin/monitor-hotplug.sh
@@ -5,22 +5,20 @@ INTNAME="eDP1"
 EXTNAME="DP1"
 EXTSTATUS=$(cat /sys/class/drm/card0-DP-1/status)
 
-displaynum=`ls /tmp/.X11-unix/* | sed s#/tmp/.X11-unix/X##`
-display=":$displaynum"
-export DISPLAY=":$displaynum"
+export DISPLAY=":0"
 
 user=$(loginctl list-users | grep -P '\d{4}' | awk '{print $2}') # Assuming uid >= 1000 #FIXME
 if [ -n "$user" ]; then
   export XAUTHORITY="/home/$user/.Xauthority"
 else
-  echo "unable to find an X session"
+  echo "Unable to find an X session."
   exit 1
 fi
 
 if [[ $EXTSTATUS = "connected" ]]; then
   xrandr --output $INTNAME --off --output $EXTNAME --auto
-  nitrogen --restore
+  nitrogen --restore || true
 elif [[ $EXTSTATUS = "disconnected" ]]; then
   xrandr --output $INTNAME --auto --output $EXTNAME --off
-  nitrogen --restore
+  nitrogen --restore || true
 fi
